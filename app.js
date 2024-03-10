e5606a37a8559870e65e11a7c744f7f0621c5b86
周家頡 <jackchou@zhoujiajiadeMacBook-Pro.local>
2024-03-10 16:28:03 +0800

    testing upload path

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(
-  "/uploads/images",
-  express.static(path.join(__dirname, "uploads/images"))
-);
+app.use("./uploads/images", express.static(path.join("uploads", "images")));
 
 app.use("/api/places", placesRoute);
 app.use("/api/users", usersRoute);
